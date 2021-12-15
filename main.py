03f264167584897cd6290b96d9ec66a087b3baa5
Prisoe <53616398+Prisoe@users.noreply.github.com>
2021-12-15 16:07:55 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.penup()
+tim.goto(-5, -5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
